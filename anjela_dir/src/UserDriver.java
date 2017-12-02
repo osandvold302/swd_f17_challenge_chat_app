136ad34b141313a0f864315a6855e4a59043de62
abubiy <anjela-bubiy@uiowa.edu>
2017-12-02 15:50:20 -0600

    updated User driver

--- anjela_dir/src/UserDriver.java
+++ anjela_dir/src/UserDriver.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
 public class UserDriver extends Application
@@ -6,6 +7,10 @@ public class UserDriver extends Application
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        User root = FXMLLoader.load(getClass().getResource("startupView.fxml"));
+    }
 
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
