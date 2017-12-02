300737612cbb7e4805a812a9b11dbb6efe18a2a2
osandvold302 <ollie302@gmail.com>
2017-12-02 12:22:49 -0600

    added the files chris has been working on - the user class

--- java_chris/src/User.java
+++ java_chris/src/User.java
@@ -5,33 +5,36 @@ import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class User {
     private ObjectInputStream input;
     private ObjectOutputStream output;
     private Socket client;
+    private String currentChannel;
+    private ArrayList<String> channels;
+    private String ID;
 
-    public User(){
+    public User(String ID){
         try{
             client = new Socket(InetAddress.getByName("127.0.0.1"), 23555);
             output = new ObjectOutputStream(client.getOutputStream());
-            output.flush();
             input = new ObjectInputStream(client.getInputStream());
+
         } catch (UnknownHostException uhe) {
             uhe.printStackTrace();
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
+        channels = new ArrayList<>();
+        this.ID = ID;
     }
 
-    /**
-     * Model. Will be used by controller. USE WHEN USER PRESSES SEND
-     * @param message message to be sent by user
-     * @return true if succesfully sent false if an error occurred
-     */
+    //Send a message from the current channel
     public boolean sendMessage(String message){
         try {
-            output.writeObject(message);
+            output.writeObject(currentChannel+","+ID+","+message);
             output.flush();
             return true;
         }catch(IOException e){
@@ -39,10 +42,82 @@ public class User {
         }
     }
 
-    public String recieveMessage(){
+    //Send a message to create a new channel with the list of users specified
+    public boolean newChannel(String channel,String[] users){
+        if(channelExists(channel)){
+            return false;
+        }else {
+            try {
+                StringBuilder builder = new StringBuilder();
+                builder.append(channel);
+                for (String user : users) {
+                    builder.append("," + user);
+                }
+                output.writeObject(builder.toString());
+                output.flush();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+            return true;
+        }
+    }
+
+    //Sees if user already is a part of a channel with this name
+    public boolean channelExists(String channel){
+        for(String chan:channels){
+            if(chan.equals(channel)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Send a message to request the history of a channel
+    public void requestMessageHistory(String channel){
         try {
-            String message = (String) input.readObject();
-            return message;
+            output.writeObject(channel + "," + "REQUEST" + "," + "REQUESTOLDMESSAGES");
+            output.flush();
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+
+    //Changes channel to specified channel and requests the message history from the server.
+    public void changeChannel(String channel){
+        currentChannel = channel;
+        requestMessageHistory(channel);
+    }
+
+
+    public String receiveMessage(){
+        try {
+            //Get information from the server and find out what group and user it is from
+            String information = (String) input.readObject();
+            Scanner splitter = new Scanner(information);
+            splitter.useDelimiter(",");
+            String channel = splitter.next();
+            String user = splitter.next();
+            String message = splitter.next();
+            StringBuilder wholeMessage = new StringBuilder();
+            wholeMessage.append(message);
+            while(splitter.hasNext()){
+                wholeMessage.append(","+splitter.next());
+            }
+            splitter.close();
+
+            if(user.equals("REQUEST"))
+            {
+                return wholeMessage.toString();
+            }
+
+            if(!channelExists(channel)){
+                addChannel(channel);
+            }
+
+            //If this channel is the one the user is currently looking at, spit out the message
+            if(channel.equals(currentChannel)) {
+                return user + " >> " + wholeMessage.toString();
+            }
         }catch (IOException ioe){
             ioe.printStackTrace();
         }catch(ClassNotFoundException cnfe) {
@@ -63,4 +138,3 @@ public class User {
 
 
 }
-
