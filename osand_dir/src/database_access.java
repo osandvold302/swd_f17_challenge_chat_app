c15cc6582f3e91ddf90b880043e07bc23d9a430a
osandvold302 <ollie302@gmail.com>
2017-12-01 23:08:50 -0600

    started on database query

---
+++ osand_dir/src/database_access.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+
+public class database_access {
+    /** database url - should not change*/
+    private static final String DATABASE_URL = "jbdc:mysql://localhost:3306/s-l112.engr.uiowa.edu";
+    /** username connection*/
+    private static final String USER = "engr_class015";
+    /** password for our team*/
+    private static final String PASS = "engr_class015-xyz";
+
+    public static void main(String[] args){
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try{
+            connection = DriverManager.getConnection(DATABASE_URL,USER,PASS);
+
+            statement = connection.createStatement();
+
+
+        }catch(SQLException sqlerr){
+
+        }
+    }
+}
