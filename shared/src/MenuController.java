cb25f94bf302ff3ecee4733130d6fb83b05d8b31
osandvold302 <olivia-sandvold@uiowa.edu>
2017-12-03 23:39:52 -0600

    I hope to god this works thanks

--- shared/src/MenuController.java
+++ shared/src/MenuController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -16,14 +17,11 @@ public class MenuController extends GeneralController {
     /** this reference to the display area where channels will appear*/
     @FXML
     private VBox messageDispArea;
-    /** this reference is to the channels the user is in*/
-    private ArrayList<String> channels;
 
     /** called when FXMLLoader loads   */
     @FXML
     public void initialize(){
         // get the channels for the user
-        //getChannels();
         for(String channel : getClient().getChannels()){ // try to add all the buttons for the channels
             Button channelDisp = new Button(channel);
             channelDisp.setOnAction(new EventHandler<ActionEvent>() {
@@ -44,14 +42,27 @@ public class MenuController extends GeneralController {
                     }
                 }
             });
+            channelDisp.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    String channelName = ((Button) event.getSource()).getText();
+                    getClient().changeChannel(channelName);
+                    MessageController.setClient(getClient());
+
+                    // change view
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("messagesView.fxml"));
+                    // once in messageView, request information
+                    try{
+                        Parent root = (Parent)loader.load();
+                        getStage().setScene(new Scene(root));
+                    }catch(IOException err){
+                        err.printStackTrace();
+                    }
+                }
+            });
             // add buttons to the VBox
             messageDispArea.getChildren().add(new Button(channel));
         }
-    }
-
-    /** getters for channels user is in */
-    private void getChannels(){
-        channels = getClient().getChannels();
     }
 
     /** listener for when the user presses the create new channel button
