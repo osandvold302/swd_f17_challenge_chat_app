0527c5e3c8fb3df8eb6fb671f26f908384b9e9ad
osandvold302 <olivia-sandvold@uiowa.edu>
2017-12-03 19:27:07 -0600

    added the wrong effing actionevent

--- shared/src/MessageController.java
+++ shared/src/MessageController.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
-import java.awt.event.ActionEvent;
+import javafx.event.ActionEvent;
 import java.io.IOException;
 import java.util.Scanner;
 
