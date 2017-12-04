c9bcad30a547364a9f1fae42208922078fa1c426
abubiy <anjela-bubiy@uiowa.edu>
2017-12-03 23:21:54 -0600

    finished RequestFulfillment java docs

--- java_chris/src/RequestFulfillment.java
+++ java_chris/src/RequestFulfillment.java
@@ -1,5 +1,17 @@
-public class RequestFulfillment extends MessageToClient {
-    public RequestFulfillment(String channel, String messageLog){
+/**
+ * creates request for message logs from
+ * specific channel
+ */
+public class RequestFulfillment extends MessageToClient
+{
+    /**
+     * constructs a request for message log
+     * @param channel the channel the message logs are
+     *                requested for
+     * @param messageLog string of messages from the channel
+     */
+    public RequestFulfillment(String channel, String messageLog)
+    {
         super(channel,messageLog);
     }
 }
