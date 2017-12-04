e1ed9e9e565c4c3d48f0499173861e755fc6f27f
abubiy <anjela-bubiy@uiowa.edu>
2017-12-03 23:47:27 -0600

    finished TextMessage javadocs

--- java_chris/src/TextMessage.java
+++ java_chris/src/TextMessage.java
@@ -1,16 +1,45 @@
-public class TextMessage extends MessageToServer {
+/**
+ * message sent to specific channel by a user
+ */
+public class TextMessage extends MessageToServer
+{
+    /**
+     * user that wrote the message
+     */
     private String user;
+
+    /**
+     * words in message
+     */
     private String message;
-    public TextMessage(String channel, String user, String message){
+
+    /**
+     * ties the user, text, and channel to
+     * a message
+     * @param channel the channel the user is on
+     * @param user the person sending the message
+     * @param message the text being sent
+     */
+    public TextMessage(String channel, String user, String message)
+    {
         super(channel);
         this.user = user;
         this.message = message;
     }
 
+    /**
+     * Getter for user
+     * @return user that sent the message
+     */
     public String getUser() {
         return user;
     }
 
+
+    /**
+     * Getter for message
+     * @return text in the message
+     */
     public String getMessage(){
         return message;
     }
